c315456ebbda246cf31b7e5aaaa6d9f1bce7a367
Tushar Choudhari <choudharit23@gmail.com>
2019-05-04 11:49:26 +0530

    transition added

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,13 +1,16 @@
 function black(){
     let background=document.querySelector("#front");
     background.style.backgroundColor= "black";
+    background.style.transition="background-color 0.5s 0.3s ease-in";
 
 
     let profBack=document.querySelector("#prof");
     profBack.style.backgroundColor="white";
     profBack.style.color="black";
+    profBack.style.transition="color 0.5s 0.3s ease-in,background-color 0.5s 0.3s ease-in"
 
     let menu= document.querySelector(".about a");
     menu.style.color="white";
+    menu.style.transition="color 0.5s 0.3s ease-in"
 
 }
