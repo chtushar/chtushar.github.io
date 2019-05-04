239025d146a312a0dd6c6d2697bc59c57414a4b9
Tushar Choudhari <choudharit23@gmail.com>
2019-05-04 20:00:03 +0530

    theme colour changed

--- JS/scroll.js
+++ JS/scroll.js
@@ -10,7 +10,7 @@ function black(){
     profBack.style.transition="color 0.5s 0.3s ease-in,background-color 0.5s 0.3s ease-in"
 
     let menu= document.querySelector(".about a");
-    menu.style.color="#F2BB15";
+    menu.style.color="#855BFF";
     menu.style.transition="color 0.5s 0.3s ease-in";
 
 }
