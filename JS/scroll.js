4165dc267e7bdfd07c131e19c7824416f2b8a910
Tushar Choudhari <choudharit23@gmail.com>
2019-05-04 10:48:06 +0530

    background of front changes on scroll

---
+++ JS/scroll.js
@@ -0,0 +1,13 @@
+function black(){
+    let background=document.querySelector("#front");
+    background.style.backgroundColor= "black";
+
+
+    let profBack=document.querySelector("#prof");
+    profBack.style.backgroundColor="white";
+    profBack.style.color="black";
+
+    let menu= document.querySelector(".about a");
+    menu.style.color="white";
+
+}
